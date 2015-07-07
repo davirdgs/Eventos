951dd6d62149e447f24c14e0f24b52aa479702b3
Davi Rodrigues <davirdgs@MacBook-Pro-de-Davi.local>
2015-07-07 10:04:06 -0300

    custom segue with animation

---
+++ StaffManager/StaffManager/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  StaffManager
+//
+//  Created by Davi Rodrigues on 07/07/15.
+//  Copyright (c) 2015 Davi Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func loginButtonHandler(sender: AnyObject) {
+        performSegueWithIdentifier("staffMode", sender: self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StaffManager/StaffManager/newCustomSegue.swift
@@ -0,0 +1,49 @@
+//
+//  newCustomSegue.swift
+//  StaffManager
+//
+//  Created by Davi Rodrigues on 07/07/15.
+//  Copyright (c) 2015 Davi Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class newCustomSegue: UIStoryboardSegue {
+    
+    override func perform() {
+        
+        let sourceView: UIView = (self.sourceViewController as! UIViewController).view
+        let destinationView: UIView = (self.destinationViewController as! UIViewController).view
+        
+        let window = UIApplication.sharedApplication().keyWindow
+        
+        destinationView.center = CGPointMake(sourceView.center.x + sourceView.frame.size.width, destinationView.center.y)
+        
+        
+        window?.insertSubview(destinationView, aboveSubview: sourceView)
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            destinationView.center = CGPointMake(sourceView.center.x, destinationView.center.y)
+            sourceView.center = CGPointMake(0 - sourceView.center.x, destinationView.center.y)
+            
+            }, completion: {
+                (result) -> Void in
+                self.sourceViewController.presentViewController(self.destinationViewController as! UIViewController, animated: false, completion: nil)
+        })
+        
+        /*
+        [UIView animateWithDuration:0.4
+            animations:^{
+            dv.center = CGPointMake(sv.center.x,
+            dv.center.y);
+            sv.center = CGPointMake(0 - sv.center.x,
+            dv.center.y);
+            }
+            completion:^(BOOL finished){
+            [[self sourceViewController] presentViewController:
+            [self destinationViewController] animated:NO completion:nil];
+            }];
+        */
+        
+    }
+   
+}
